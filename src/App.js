7371b8a1d080e69df56e18f11739cab3fafe8474
MarianP24 <p.marian24@yahoo.com>
2025-03-06 18:32:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import BookList from './components/BookList';
+import Login from './components/Login';
+import Register from './components/Register';
+import PrivateRoute from './components/PrivateRoute';
+import AdminRoute from './components/AdminRoute';
+import UserManagement from './components/UserManagement';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* Standard user routes */}
+            <Route element={<PrivateRoute />}>
+              <Route path="/books" element={
+                <>
+                  <Navbar />
+                  <BookList />
+                </>
+              } />
+            </Route>
+
+            {/* Admin-only routes */}
+            <Route element={<AdminRoute />}>
+              <Route path="/admin/users" element={
+                <>
+                  <Navbar />
+                  <UserManagement />
+                </>
+              } />
+            </Route>
+
+            <Route path="/" element={<Navigate to="/books" replace />} />
+            <Route path="*" element={<Navigate to="/books" replace />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
